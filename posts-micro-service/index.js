8681c869b6c480efc2ff41ac21df9dbe735802fd
Rajat Pandey <rajat.pandey@optus.com.au>
2020-07-18 18:38:51 +1000

    POST and CREATED Event

--- posts-micro-service/index.js
+++ posts-micro-service/index.js
@@ -1,26 +1,38 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-
+app.get('/posts', async (req, res) => {
+ res.status(200).send(posts);
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const { title } = req.body;
   const id = randomBytes(4).toString('hex');
+  const body = { id, title, 'type': 'POST_CREATED' };
+  const resp = await axios.post('http://localhost:4005/events', body);
+  res.status(200).send(resp.data);
+})
+
+app.post('/events', async (req, res) => {
+  const eventDetails = req.body;
+  const id = eventDetails.id;
   posts[id] = {
       id,
-      title
+      title: eventDetails.title
   }
-  res.status(201).send(posts[id]);
+  res.status(201).send(posts);
 })
 
 app.listen('4000', () => {
-    console.log('Listening on Port 4000');
+    console.log('Post service listening on port 4000');
 })
